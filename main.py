77c02212e0f9d6ed90e34ab223dffe2420675a43
xyw00 <xyw00@speechlab.sjtu.edu.cn>
2016-04-30 12:14:43 +0800

    reader.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
+#!/usr/bin/python
 # Author: CodePothunter
 # Date:   2016-04-30
+
--- utils/reader.py
+++ utils/reader.py
@@ -11,9 +11,20 @@ class reader(object):
     """
     def __init__(self, filename):
         self.file = filename
+        self.features = []
+        self.targets = []
 
     def get_data(self):
-        pass
+        read_in = pandas.read_csv(self.file)
+        for i in xrange(len(read_in)):
+        	feature = []
+        	target = 0
+        	for key in read_in.columns.values:
+        		feature.append(reader[key][i])
+    		target = feature[-1]
+    		feature = feature[:-1]
+    		self.features.append(feature)
+    		self.targets.append(target)
 
     def get_buffer(self):
         pass
